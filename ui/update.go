fd0648ac1d64212f67b36e876033053bc72c0c0b
knipferrc <knipferrc@gmail.com>
2021-05-30 17:49:23 -0400

    add logging

--- ui/update.go
+++ ui/update.go
@@ -147,6 +147,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 
 	case tea.KeyMsg:
+		log.Output(2, msg.String())
+
 		if msg.String() == "g" && m.PreviousKey.String() == "g" {
 			if !m.ShowCommandBar {
 				if m.PrimaryPane.IsActive {
