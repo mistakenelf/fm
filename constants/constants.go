96aee6d92bbe4393100681d0d10dff402cbe5c4c
knipferrc <knipferrc@gmail.com>
2021-06-07 07:34:41 -0400

    move help text to constant

--- constants/constants.go
+++ constants/constants.go
@@ -12,4 +12,23 @@ const (
 	LightPurple = "#A550DF"
 	DarkPurple  = "#6124DF"
 	DarkGray    = "#353533"
+
+	HelpText = `# FM (File Manager)
+- h or left arrow     | go back a directory
+- j or down arrow     | move cursor down
+- k or up arrow       | move cursor up
+- l or right arrow    | open selected folder / view file
+- gg                  | go to top of pane
+- G                   | go to botom of pane
+- ~                   | switch to home directory
+- .                   | toggle hidden files and directories
+- (-)                   | Go To previous directory
+- :                   | open command bar
+- mkdir dirname       | create directory in current directory
+- touch filename.txt  | create file in current directory
+- mv newname.txt      | rename currently selected file or directory
+- cp /dir/to/move/to  | move file or directory
+- rm                 | remove file or directory
+- tab                | toggle between panes
+	`
 )
