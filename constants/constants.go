864d7bab48e7c195ea3182a3f4561781863ed8d9
knipferrc <knipferrc@gmail.com>
2021-06-13 09:23:17 -0400

    update help text

--- constants/constants.go
+++ constants/constants.go
@@ -15,21 +15,27 @@ const (
 	Blue        = "#1D4ED8"
 
 	HelpText = `# FM (File Manager)
-- h or left arrow               | go back a directory
-- j or down arrow               | move cursor down
-- k or up arrow                 | move cursor up
-- l or right arrow              | open selected folder / view file
-- gg                            | go to top of pane
-- G                             | go to botom of pane
-- ~                             | switch to home directory
-- .                             | toggle hidden files and directories
-- (-)                           | Go To previous directory
-- :                             | open command bar
-- m                             | enter into move mode
-- mkdir dirname                 | create directory in current directory
-- touch filename.txt            | create file in current directory
-- mv or rename newname.txt      | rename currently selected file or directory
-- delete or rm                  | remove file or directory
-- tab                           | toggle between panes
+| Key                | Description                                                                                                                                                                                                                                                      |
+| ------------------ | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| h or left          | Go back to previous directory                                                                                                                                                                                                                                    |
+| j or down          | Move down in the file tree or scroll pane down                                                                                                                                                                                                                   |
+| k or up            | Move up in the file tree or scroll pane up                                                                                                                                                                                                                       |
+| l or right         | Opens the currently selected directory or file                                                                                                                                                                                                                   |
+| gg                 | Jump to bottom of file tree or pane                                                                                                                                                                                                                              |
+| G                  | Jump to top of file tree or pane                                                                                                                                                                                                                                 |
+| ~                  | Go to home directory                                                                                                                                                                                                                                             |
+| .                  | Toggle hide files and directories                                                                                                                                                                                                                                |
+| -                  | Go to previous directory                                                                                                                                                                                                                                         |
+| ctrl+c             | Exit                                                                                                                                                                                                                                                             |
+| q                  | Exit if command bar is not open                                                                                                                                                                                                                                  |
+| m                  | Move the currently selected file or directory. Once pressed, the file manager enters move mode. Navigate the tree as usual and press enter in the desired destination directory. It will navigate back to the starting direcotry in which the move was initiated |
+| :                  | Open command bar                                                                                                                                                                                                                                                 |
+| mkdir dirname      | Create a new directory in the current directory                                                                                                                                                                                                                  |
+| touch filename.txt | Create a new file in the current directory                                                                                                                                                                                                                       |
+| rename or mv       | Rename currently selected file or directory                                                                                                                                                                                                                      |
+| delete or rm       | Delete the currently selected file or directory                                                                                                                                                                                                                  |
+| tab                | Toggle between panes                                                                                                                                                                                                                                             |
+| esc                | Cancel any current action. Pressing escape during any action (rename, move, delete) will cancel that operation and return back to file navigation                                                                                                                |                          
+| tab                | toggle between panes
 	`
 )
