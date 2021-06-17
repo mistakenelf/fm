488f01d970af39a971b92a5af87f494cf06573be
knipferrc <knipferrc@gmail.com>
2021-06-17 16:09:39 -0400

    add comments and check if startDir is valid

--- app/app.go
+++ app/app.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"fmt"
 	"io/fs"
 	"log"
 	"os"
@@ -15,6 +14,8 @@ import (
 )
 
 func Run() {
+	// Setup default and load the users
+	// app configuration
 	config.SetDefaults()
 	config.LoadConfig()
 
@@ -23,42 +24,69 @@ func Run() {
 	var files []fs.FileInfo
 	var startDir string
 
+	// If logging is enabled in the config, log to the
+	// debug.log file
 	if cfg.Settings.EnableLogging {
 		f, err := tea.LogToFile("debug.log", "debug")
 		if err != nil {
-			fmt.Println("fatal:", err)
+			log.Fatal(err)
 			os.Exit(1)
 		}
 
 		defer f.Close()
 	}
 
+	// FM can be started with a starting directory as an argument,
+	// check if an argument exists and use it as a starting directory
 	if len(os.Args) > 1 {
 		startDir = os.Args[1]
 	}
 
-	if startDir != "" {
-		files, _ = utils.GetDirectoryListing(startDir, true)
+	// Get the initial directory listing to be displayed
+	if _, err := os.Stat(startDir); err == nil {
+		files, err = utils.GetDirectoryListing(startDir, true)
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else if cfg.Settings.StartDir == constants.HomeDirectory {
-		homeDir, _ := utils.GetHomeDirectory()
-		files, _ = utils.GetDirectoryListing(homeDir, true)
+		homeDir, err := utils.GetHomeDirectory()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		files, err = utils.GetDirectoryListing(homeDir, true)
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else if _, err := os.Stat(cfg.Settings.StartDir); err == nil {
-		files, _ = utils.GetDirectoryListing(cfg.Settings.StartDir, true)
+		files, err = utils.GetDirectoryListing(cfg.Settings.StartDir, true)
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else {
-		files, _ = utils.GetDirectoryListing(constants.CurrentDirectory, true)
+		files, err = utils.GetDirectoryListing(constants.CurrentDirectory, true)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
+	// Create new model of UI passing it the inital direcotry listing
 	m := ui.NewModel(files)
 
 	var opts []tea.ProgramOption
+
+	// Always append alt screen program option
 	opts = append(opts, tea.WithAltScreen())
 
+	// If mousewheel is enabled, append it to the program options
 	if cfg.Settings.EnableMouseWheel {
 		opts = append(opts, tea.WithMouseAllMotion())
 	}
 
+	// Initialize new app
 	p := tea.NewProgram(m, opts...)
 
+	// If the program fails to start, exit the program
 	if err := p.Start(); err != nil {
 		log.Fatal("Failed to start fm", err)
 		os.Exit(1)
