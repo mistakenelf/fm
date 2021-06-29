fde04b879e73c5109d15bb90d8822d66e475fd28
knipferrc <knipferrc@gmail.com>
2021-06-29 12:33:17 -0400

    setup cobra and add version command

--- app/app.go
+++ app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"io/fs"
 	"log"
 	"os"
@@ -11,82 +12,96 @@ import (
 	"github.com/knipferrc/fm/utils"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
 )
 
-func Run() {
-	// Setup default config and load the users config
-	config.SetDefaults()
-	config.LoadConfig()
+var rootCmd = &cobra.Command{
+	Use:     "fm",
+	Short:   "FM is a simple, configurable and fun to use file manager",
+	Version: "v0.0.5",
+	Args:    cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		// Setup default config and load the users config
+		config.SetDefaults()
+		config.LoadConfig()
 
-	cfg := config.GetConfig()
+		cfg := config.GetConfig()
 
-	var files []fs.FileInfo
-	var startDir string
+		var files []fs.FileInfo
+		var startDir string
 
-	// If logging is enabled, logs will be output to debug.log
-	if cfg.Settings.EnableLogging {
-		f, err := tea.LogToFile("debug.log", "debug")
-		if err != nil {
-			log.Fatal(err)
+		// If logging is enabled, logs will be output to debug.log
+		if cfg.Settings.EnableLogging {
+			f, err := tea.LogToFile("debug.log", "debug")
+			if err != nil {
+				log.Fatal(err)
+				os.Exit(1)
+			}
+
+			defer f.Close()
+		}
+
+		// FM can be started with a starting directory as an argument,
+		// check if an argument exists and use it as the starting directory
+		if len(os.Args) > 1 {
+			startDir = os.Args[1]
+		}
+
+		// Get the initial directory listing to be displayed
+		if _, err := os.Stat(startDir); err == nil {
+			files, err = utils.GetDirectoryListing(startDir, true)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else if cfg.Settings.StartDir == constants.HomeDirectory {
+			homeDir, err := utils.GetHomeDirectory()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			files, err = utils.GetDirectoryListing(homeDir, true)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else if _, err := os.Stat(cfg.Settings.StartDir); err == nil {
+			files, err = utils.GetDirectoryListing(cfg.Settings.StartDir, true)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			files, err = utils.GetDirectoryListing(constants.CurrentDirectory, true)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		// Create new model of UI passing it the inital direcotry listing
+		m := ui.NewModel(files)
+
+		var opts []tea.ProgramOption
+
+		// Always append alt screen program option
+		opts = append(opts, tea.WithAltScreen())
+
+		// If mousewheel is enabled, append it to the program options
+		if cfg.Settings.EnableMouseWheel {
+			opts = append(opts, tea.WithMouseAllMotion())
+		}
+
+		// Initialize new app
+		p := tea.NewProgram(m, opts...)
+
+		// If the program fails to start, exit the program
+		if err := p.Start(); err != nil {
+			log.Fatal("Failed to start fm", err)
 			os.Exit(1)
 		}
+	},
+}
 
-		defer f.Close()
-	}
-
-	// FM can be started with a starting directory as an argument,
-	// check if an argument exists and use it as the starting directory
-	if len(os.Args) > 1 {
-		startDir = os.Args[1]
-	}
-
-	// Get the initial directory listing to be displayed
-	if _, err := os.Stat(startDir); err == nil {
-		files, err = utils.GetDirectoryListing(startDir, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else if cfg.Settings.StartDir == constants.HomeDirectory {
-		homeDir, err := utils.GetHomeDirectory()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		files, err = utils.GetDirectoryListing(homeDir, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else if _, err := os.Stat(cfg.Settings.StartDir); err == nil {
-		files, err = utils.GetDirectoryListing(cfg.Settings.StartDir, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		files, err = utils.GetDirectoryListing(constants.CurrentDirectory, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Create new model of UI passing it the inital direcotry listing
-	m := ui.NewModel(files)
-
-	var opts []tea.ProgramOption
-
-	// Always append alt screen program option
-	opts = append(opts, tea.WithAltScreen())
-
-	// If mousewheel is enabled, append it to the program options
-	if cfg.Settings.EnableMouseWheel {
-		opts = append(opts, tea.WithMouseAllMotion())
-	}
-
-	// Initialize new app
-	p := tea.NewProgram(m, opts...)
-
-	// If the program fails to start, exit the program
-	if err := p.Start(); err != nil {
-		log.Fatal("Failed to start fm", err)
+func Run() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
