5cd3edc824a7bc81d355ce1bda6c440409c3298e
knipferrc <knipferrc@gmail.com>
2021-10-10 09:38:36 -0400

    v0.5.5

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "fm",
 	Short:   "FM is a simple, configurable, and fun to use file manager",
-	Version: "0.5.4",
+	Version: "0.5.5",
 	Args:    cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		config.LoadConfig()
