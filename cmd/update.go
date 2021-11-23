a2a13d0e6c2428335570860dd7fbb9e7eb3725ae
knipferrc <knipferrc@gmail.com>
2021-11-23 09:21:29 -0500

    add self update command

---
+++ cmd/update.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update FM to the latest version",
+	Long:  `Update FM to the latest version.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		updateCommand := exec.Command("bash", "-c", "curl -sfL https://raw.githubusercontent.com/knipferrc/fm/main/install.sh | sh")
+		updateCommand.Stdin = os.Stdin
+		updateCommand.Stdout = os.Stdout
+		updateCommand.Stderr = os.Stderr
+
+		err := updateCommand.Run()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		os.Exit(0)
+	},
+}
