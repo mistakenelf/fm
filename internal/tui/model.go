cd87b6f3fe5733aabddee56c79785b1300568dcd
knipferrc <knipferrc@gmail.com>
2022-04-03 19:18:56 -0400

    fix: use borderless from config

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -1,7 +1,10 @@
 package tui
 
 import (
+	"log"
+
 	"github.com/charmbracelet/lipgloss"
+	"github.com/knipferrc/fm/internal/config"
 	"github.com/knipferrc/teacup/code"
 	"github.com/knipferrc/teacup/filetree"
 	"github.com/knipferrc/teacup/help"
@@ -36,39 +39,42 @@ type Bubble struct {
 
 // New creates a new instance of the UI.
 func New() Bubble {
-	filetreeModel := filetree.New(true, false, lipgloss.AdaptiveColor{Light: "#000000", Dark: "63"})
-	codeModel := code.New(false, false, lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"})
-	imageModel := image.New(false, false, lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"})
-	markdownModel := markdown.New(false, false, lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"})
-	pdfModel := pdf.New(false, false, lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"})
+	cfg, err := config.ParseConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filetreeModel := filetree.New(true, cfg.Settings.Borderless, lipgloss.AdaptiveColor{Light: "#000000", Dark: "63"})
+	codeModel := code.New(false, cfg.Settings.Borderless, lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"})
+	imageModel := image.New(false, cfg.Settings.Borderless, lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"})
+	markdownModel := markdown.New(false, cfg.Settings.Borderless, lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"})
+	pdfModel := pdf.New(false, cfg.Settings.Borderless, lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"})
 	helpModel := help.New(
 		lipgloss.AdaptiveColor{Light: "#000000", Dark: "#ffffff"},
 		"Help",
 		[]help.Entry{
-			{Key: "ctrl+c", Description: "Exit FM"},
+			{Key: "ctrl+c, q", Description: "Exit FM"},
 			{Key: "j/up", Description: "Move up"},
 			{Key: "k/down", Description: "Move down"},
-			{Key: "h/left", Description: "Go back a directory"},
-			{Key: "l/right", Description: "Read file or enter directory"},
-			{Key: "p", Description: "Preview directory"},
+			{Key: "space", Description: "Read file or enter directory"},
 			{Key: "G", Description: "Jump to bottom"},
+			{Key: "g", Description: "Jump to top"},
 			{Key: "~", Description: "Go to home directory"},
 			{Key: ".", Description: "Toggle hidden files"},
 			{Key: "y", Description: "Copy file path to clipboard"},
-			{Key: "Z", Description: "Zip currently selected tree item"},
-			{Key: "U", Description: "Unzip currently selected tree item"},
+			{Key: "z", Description: "Zip currently selected tree item"},
+			{Key: "u", Description: "Unzip currently selected tree item"},
 			{Key: "n", Description: "Create new file"},
 			{Key: "N", Description: "Create new directory"},
-			{Key: "ctrl+d", Description: "Delete currently selected tree item"},
-			{Key: "M", Description: "Move currently selected tree item"},
+			{Key: "x", Description: "Delete currently selected tree item"},
 			{Key: "enter", Description: "Process command"},
 			{Key: "E", Description: "Edit currently selected tree item"},
-			{Key: "C", Description: "Copy currently selected tree item"},
-			{Key: "esc", Description: "Reset FM to initial state"},
+			{Key: "c", Description: "Copy currently selected tree item"},
+			{Key: "esc", Description: "Reset input field"},
 			{Key: "tab", Description: "Toggle between boxes"},
 		},
 		false,
-		false,
+		cfg.Settings.Borderless,
 	)
 
 	return Bubble{
