b1050dad656142a6cc69a921c555c9e56cb7a273
knipferrc <knipferrc@gmail.com>
2022-04-22 09:36:32 -0400

    chore: cleanup init

--- internal/tui/init.go
+++ internal/tui/init.go
@@ -7,8 +7,5 @@ import (
 // Init initializes the UI and sets up initial data.
 // Init intializes the UI.
 func (b Bubble) Init() tea.Cmd {
-	initTreeCmd := b.filetree.Init()
-	toggleTreeIconsCmd := b.filetree.ToggleShowIcons(b.config.Settings.ShowIcons)
-
-	return tea.Sequentially(initTreeCmd, toggleTreeIconsCmd)
+	return b.filetree.Init()
 }
