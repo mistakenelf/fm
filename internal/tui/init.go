ad6c5c34c2f84db54d657dcf65d9810b38964f8a
knipferrc <knipferrc@gmail.com>
2022-04-26 13:07:11 -0400

    chore: lint

--- internal/tui/init.go
+++ internal/tui/init.go
@@ -4,7 +4,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// Init initializes the UI and sets up initial data.
 // Init intializes the UI.
 func (b Bubble) Init() tea.Cmd {
 	return b.filetree.Init()
