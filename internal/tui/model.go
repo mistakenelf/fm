40c18291efe7594b3e8d44fe360f85332a0ab232
mistakenelf <tyler.knipfer@icloud.com>
2024-03-30 19:57:53 -0400

    feat: shared keys

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -11,6 +11,7 @@ import (
 	"github.com/mistakenelf/fm/icons"
 	"github.com/mistakenelf/fm/image"
 	"github.com/mistakenelf/fm/internal/theme"
+	"github.com/mistakenelf/fm/keys"
 	"github.com/mistakenelf/fm/markdown"
 	"github.com/mistakenelf/fm/pdf"
 	"github.com/mistakenelf/fm/statusbar"
@@ -48,7 +49,7 @@ type model struct {
 	pdf                   pdf.Model
 	statusbar             statusbar.Model
 	state                 sessionState
-	keyMap                keyMap
+	keyMap                keys.KeyMap
 	activePane            int
 	height                int
 	config                Config
@@ -109,6 +110,8 @@ func New(cfg Config) model {
 		},
 	)
 
+	defaultKeyMap := keys.DefaultKeyMap()
+
 	helpModel := help.New(
 		"Help",
 		help.TitleColor{
@@ -116,33 +119,36 @@ func New(cfg Config) model {
 			Foreground: cfg.Theme.TitleForegroundColor,
 		},
 		[]help.Entry{
-			{Key: "ctrl+c, q", Description: "Exit FM"},
-			{Key: "j/up", Description: "Move up"},
-			{Key: "k/down", Description: "Move down"},
-			{Key: "h", Description: "Go to previous directory"},
-			{Key: "l", Description: "Open file or directory"},
-			{Key: "G", Description: "Jump to bottom"},
-			{Key: "g", Description: "Jump to top"},
-			{Key: "~", Description: "Go to home directory"},
-			{Key: ".", Description: "Toggle hidden files"},
-			{Key: "y", Description: "Copy file path to clipboard"},
-			{Key: "z", Description: "Zip currently selected tree item"},
-			{Key: "u", Description: "Unzip currently selected tree item"},
-			{Key: "N", Description: "Create new file"},
-			{Key: "M", Description: "Create new directory"},
-			{Key: "X", Description: "Delete currently selected tree item"},
-			{Key: "m", Description: "Move currently selected tree item"},
-			{Key: "enter", Description: "Submit text input"},
-			{Key: "e", Description: "Open file in $EDITOR"},
-			{Key: "C", Description: "Copy currently selected tree item"},
-			{Key: "esc", Description: "Reset state and show help"},
-			{Key: "/", Description: "Go to root directory"},
-			{Key: "tab", Description: "Toggle between panes"},
-			{Key: "K", Description: "Page up in filetree"},
-			{Key: "J", Description: "Page down in filetree"},
-			{Key: "D", Description: "Only show directories in filetree"},
-			{Key: "F", Description: "Only show files in filetree"},
-			{Key: "W", Description: "Write selection path to file if its set"},
+			{Key: defaultKeyMap.ForceQuit.Help().Key, Description: defaultKeyMap.ForceQuit.Help().Desc},
+			{Key: defaultKeyMap.Quit.Help().Key, Description: defaultKeyMap.Quit.Help().Desc},
+			{Key: defaultKeyMap.TogglePane.Help().Key, Description: defaultKeyMap.TogglePane.Help().Desc},
+			{Key: defaultKeyMap.OpenFile.Help().Key, Description: defaultKeyMap.OpenFile.Help().Desc},
+			{Key: defaultKeyMap.ResetState.Help().Key, Description: defaultKeyMap.ResetState.Help().Desc},
+			{Key: defaultKeyMap.ShowTextInput.Help().Key, Description: defaultKeyMap.ShowTextInput.Help().Desc},
+			{Key: defaultKeyMap.Submit.Help().Key, Description: defaultKeyMap.Submit.Help().Desc},
+			{Key: defaultKeyMap.GotoTop.Help().Key, Description: defaultKeyMap.GotoTop.Help().Desc},
+			{Key: defaultKeyMap.GotoBottom.Help().Key, Description: defaultKeyMap.GotoBottom.Help().Desc},
+			{Key: defaultKeyMap.MoveDirectoryItem.Help().Key, Description: defaultKeyMap.MoveDirectoryItem.Help().Desc},
+			{Key: defaultKeyMap.Down.Help().Key, Description: defaultKeyMap.Down.Help().Desc},
+			{Key: defaultKeyMap.Up.Help().Key, Description: defaultKeyMap.Up.Help().Desc},
+			{Key: defaultKeyMap.PageUp.Help().Key, Description: defaultKeyMap.PageUp.Help().Desc},
+			{Key: defaultKeyMap.PageDown.Help().Key, Description: defaultKeyMap.PageDown.Help().Desc},
+			{Key: defaultKeyMap.GoToHomeDirectory.Help().Key, Description: defaultKeyMap.GoToHomeDirectory.Help().Desc},
+			{Key: defaultKeyMap.GoToRootDirectory.Help().Key, Description: defaultKeyMap.GoToRootDirectory.Help().Desc},
+			{Key: defaultKeyMap.ToggleHidden.Help().Key, Description: defaultKeyMap.ToggleHidden.Help().Desc},
+			{Key: defaultKeyMap.OpenDirectory.Help().Key, Description: defaultKeyMap.OpenDirectory.Help().Desc},
+			{Key: defaultKeyMap.PreviousDirectory.Help().Key, Description: defaultKeyMap.PreviousDirectory.Help().Desc},
+			{Key: defaultKeyMap.CopyPathToClipboard.Help().Key, Description: defaultKeyMap.CopyPathToClipboard.Help().Desc},
+			{Key: defaultKeyMap.CopyDirectoryItem.Help().Key, Description: defaultKeyMap.CopyDirectoryItem.Help().Desc},
+			{Key: defaultKeyMap.DeleteDirectoryItem.Help().Key, Description: defaultKeyMap.DeleteDirectoryItem.Help().Desc},
+			{Key: defaultKeyMap.ZipDirectoryItem.Help().Key, Description: defaultKeyMap.ZipDirectoryItem.Help().Desc},
+			{Key: defaultKeyMap.UnzipDirectoryItem.Help().Key, Description: defaultKeyMap.UnzipDirectoryItem.Help().Desc},
+			{Key: defaultKeyMap.ShowDirectoriesOnly.Help().Key, Description: defaultKeyMap.ShowDirectoriesOnly.Help().Desc},
+			{Key: defaultKeyMap.ShowFilesOnly.Help().Key, Description: defaultKeyMap.ShowFilesOnly.Help().Desc},
+			{Key: defaultKeyMap.WriteSelectionPath.Help().Key, Description: defaultKeyMap.WriteSelectionPath.Help().Desc},
+			{Key: defaultKeyMap.OpenInEditor.Help().Key, Description: defaultKeyMap.OpenInEditor.Help().Desc},
+			{Key: defaultKeyMap.CreateFile.Help().Key, Description: defaultKeyMap.CreateFile.Help().Desc},
+			{Key: defaultKeyMap.CreateDirectory.Help().Key, Description: defaultKeyMap.CreateDirectory.Help().Desc},
 		},
 	)
 	helpModel.SetViewportDisabled(true)
@@ -159,7 +165,7 @@ func New(cfg Config) model {
 		pdf:                   pdfModel,
 		statusbar:             statusbarModel,
 		config:                cfg,
-		keyMap:                defaultKeyMap(),
+		keyMap:                defaultKeyMap,
 		showTextInput:         false,
 		textinput:             textInput,
 		statusMessageLifetime: time.Second,
---
+++ keys/keys.go
@@ -0,0 +1,71 @@
+package keys
+
+import "github.com/charmbracelet/bubbles/key"
+
+type KeyMap struct {
+	Down                key.Binding
+	Up                  key.Binding
+	PageUp              key.Binding
+	PageDown            key.Binding
+	GoToHomeDirectory   key.Binding
+	GoToRootDirectory   key.Binding
+	ToggleHidden        key.Binding
+	OpenDirectory       key.Binding
+	PreviousDirectory   key.Binding
+	CopyPathToClipboard key.Binding
+	CopyDirectoryItem   key.Binding
+	DeleteDirectoryItem key.Binding
+	ZipDirectoryItem    key.Binding
+	UnzipDirectoryItem  key.Binding
+	ShowDirectoriesOnly key.Binding
+	ShowFilesOnly       key.Binding
+	WriteSelectionPath  key.Binding
+	OpenInEditor        key.Binding
+	CreateFile          key.Binding
+	CreateDirectory     key.Binding
+	ForceQuit           key.Binding
+	Quit                key.Binding
+	TogglePane          key.Binding
+	OpenFile            key.Binding
+	ResetState          key.Binding
+	ShowTextInput       key.Binding
+	Submit              key.Binding
+	GotoTop             key.Binding
+	GotoBottom          key.Binding
+	MoveDirectoryItem   key.Binding
+}
+
+func DefaultKeyMap() KeyMap {
+	return KeyMap{
+		ForceQuit:           key.NewBinding(key.WithKeys("ctrl+c"), key.WithHelp("ctrl+c", "Force Quit")),
+		Quit:                key.NewBinding(key.WithKeys("q"), key.WithHelp("q", "Quit when not performing action")),
+		TogglePane:          key.NewBinding(key.WithKeys("tab"), key.WithHelp("tab", "Toggle between l/r panes")),
+		OpenFile:            key.NewBinding(key.WithKeys("l", "right"), key.WithHelp("l", "Preview file")),
+		ResetState:          key.NewBinding(key.WithKeys("esc"), key.WithHelp("esc", "Reset state")),
+		ShowTextInput:       key.NewBinding(key.WithKeys("N", "M"), key.WithHelp("N, M", "Show text input to create file or directory")),
+		Submit:              key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "Submit text input value")),
+		GotoTop:             key.NewBinding(key.WithKeys("g"), key.WithHelp("g", "Go to top of pane")),
+		GotoBottom:          key.NewBinding(key.WithKeys("G"), key.WithHelp("G", "Go to bottom of pane")),
+		MoveDirectoryItem:   key.NewBinding(key.WithKeys("m"), key.WithHelp("m", "Move directory item")),
+		Down:                key.NewBinding(key.WithKeys("j", "down", "ctrl+n"), key.WithHelp("j", "Go down")),
+		Up:                  key.NewBinding(key.WithKeys("k", "up", "ctrl+p"), key.WithHelp("k", "Go up")),
+		PageUp:              key.NewBinding(key.WithKeys("K", "pgup"), key.WithHelp("pgup", "Page up")),
+		PageDown:            key.NewBinding(key.WithKeys("J", "pgdown"), key.WithHelp("pgdown", "Page down")),
+		GoToHomeDirectory:   key.NewBinding(key.WithKeys("~"), key.WithHelp("~", "Go to home directory")),
+		GoToRootDirectory:   key.NewBinding(key.WithKeys("/"), key.WithHelp("/", "Go to root directory")),
+		ToggleHidden:        key.NewBinding(key.WithKeys("."), key.WithHelp(".", "Toggle hidden files/folders")),
+		OpenDirectory:       key.NewBinding(key.WithKeys("l", "right"), key.WithHelp("l", "Open directory")),
+		PreviousDirectory:   key.NewBinding(key.WithKeys("h", "left"), key.WithHelp("h", "Go to previous directory")),
+		CopyPathToClipboard: key.NewBinding(key.WithKeys("y"), key.WithHelp("y", "Copy path to clipboard")),
+		CopyDirectoryItem:   key.NewBinding(key.WithKeys("C"), key.WithHelp("C", "Copy directory item")),
+		DeleteDirectoryItem: key.NewBinding(key.WithKeys("X"), key.WithHelp("X", "Delete directory item")),
+		ZipDirectoryItem:    key.NewBinding(key.WithKeys("Z"), key.WithHelp("Z", "Zip directory item")),
+		UnzipDirectoryItem:  key.NewBinding(key.WithKeys("U"), key.WithHelp("U", "Unzip directory item")),
+		ShowDirectoriesOnly: key.NewBinding(key.WithKeys("D"), key.WithHelp("D", "Show directories only")),
+		ShowFilesOnly:       key.NewBinding(key.WithKeys("F"), key.WithHelp("F", "Show files only")),
+		WriteSelectionPath:  key.NewBinding(key.WithKeys("W"), key.WithHelp("W", "Write selection path and quit")),
+		OpenInEditor:        key.NewBinding(key.WithKeys("e"), key.WithHelp("e", "Open in $EDITOR")),
+		CreateFile:          key.NewBinding(key.WithKeys("N"), key.WithHelp("N", "Create new file")),
+		CreateDirectory:     key.NewBinding(key.WithKeys("M"), key.WithHelp("M", "Create new directory")),
+	}
+}
